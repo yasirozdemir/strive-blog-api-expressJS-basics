ad4491b8bc2d1d445c742e52fab99f51226fd491
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-28 19:13:25 +0300

    cors uninstalled

--- src/server.js
+++ src/server.js
@@ -1,20 +1,13 @@
 import Express from "express";
 import listEndpoints from "express-list-endpoints";
-import authorsReducer from "./api/authors/index.js";
-import cors from "cors";
+import authorsRouter from "./api/authors/index.js";
 
 const server = Express();
 const port = 3001;
 
 server.use(Express.json()); //should be added BEFORE the endpoints, otherwise all the request bodies will be undefined!
 
-server.use("/authors", authorsReducer);
-
-server.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+server.use("/authors", authorsRouter);
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
