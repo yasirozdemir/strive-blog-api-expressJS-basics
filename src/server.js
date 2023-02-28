58d36b99e1d6855d82635a80141a6da97cb6932b
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-28 19:27:04 +0300

    corse installed

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./api/authors/index.js";
+import cors from "cors";
 
 const server = Express();
 const port = 3001;
+
+server.use(cors());
 
 server.use(Express.json()); //should be added BEFORE the endpoints, otherwise all the request bodies will be undefined!
 
