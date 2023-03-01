7b8065806d656a43d2480b90473886733151b016
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-01 17:10:42 +0300

    logical update: authorsRouter

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -18,21 +18,6 @@ const authorsJSONPath = join(
   "authors.json"
 );
 
-// GET (ALL THE AUTHORS)
-authorsRouter.get("/", (request, response) => {
-  const fileContentRaw = fs.readFileSync(authorsJSONPath);
-  // console.log(fileContentRaw); // fileContentRow <Buffer 5b 7b 22 6e 61... -> we should convert it into something readible and understandable by people using JSON.parse
-  const authors = JSON.parse(fileContentRaw);
-  response.send(authors);
-});
-
-// GET (SINGLE AUTHOR)
-authorsRouter.get("/:authorId", (request, response) => {
-  const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
-  const author = authors.find((a) => a.id === request.params.authorId); // find returns the specific author that we searched for
-  response.send(author);
-});
-
 // POST
 authorsRouter.post("/", (request, response) => {
   const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
@@ -54,6 +39,21 @@ authorsRouter.post("/", (request, response) => {
     fs.writeFileSync(authorsJSONPath, JSON.stringify(authors)); // updating authors.json with last added author
     response.status(201).send({ id: newAuthor.id }); // 201 -> OK, created!
   }
+});
+
+// GET (ALL THE AUTHORS)
+authorsRouter.get("/", (request, response) => {
+  const fileContentRaw = fs.readFileSync(authorsJSONPath);
+  // console.log(fileContentRaw); // fileContentRow <Buffer 5b 7b 22 6e 61... -> we should convert it into something readible and understandable by people using JSON.parse
+  const authors = JSON.parse(fileContentRaw);
+  response.send(authors);
+});
+
+// GET (SINGLE AUTHOR)
+authorsRouter.get("/:authorId", (request, response) => {
+  const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
+  const author = authors.find((a) => a.id === request.params.authorId); // find returns the specific author that we searched for
+  response.send(author);
 });
 
 // PUT (UPDATE A SINGLE AUTHOR)
