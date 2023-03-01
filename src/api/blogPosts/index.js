1e10256268fd196cadc1ccfe48eadd8b4a662c4c
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-01 16:48:09 +0300

    POST works

---
+++ src/api/blogPosts/index.js
@@ -0,0 +1,38 @@
+import Express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import uniqid from "uniqid";
+
+const blogPostsRouter = Express.Router();
+
+const blogPostsJSONPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "blogPosts.json"
+);
+
+const getBlogPosts = () => JSON.parse(fs.readFileSync(blogPostsJSONPath));
+const writeBlogPost = (blogPosts) =>
+  fs.writeFileSync(blogPostsJSONPath, JSON.stringify(blogPosts));
+
+blogPostsRouter.post("/", (req, res, next) => {
+  try {
+    const blogPosts = getBlogPosts();
+    const newBlogPost = {
+      ...req.body,
+      author: {
+        ...req.body.author,
+        avatar: `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`,
+      },
+      createdAt: new Date(),
+      id: uniqid(),
+    };
+    blogPosts.push(newBlogPost);
+    writeBlogPost(blogPosts);
+    res.status(201).send({ postId: newBlogPost.id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default blogPostsRouter;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./api/authors/index.js";
+import blogPostsRouter from "./api/blogPosts/index.js";
 import cors from "cors";
+import {
+  badRequestHandler,
+  unauthorizedHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
 
 const server = Express();
 const port = 3001;
 
 server.use(cors());
 
+// Middlewares
 const informativeMiddleware = (req, res, next) => {
   // an example of global middleware
   console.table({
@@ -20,7 +28,15 @@ const informativeMiddleware = (req, res, next) => {
 server.use(informativeMiddleware);
 server.use(Express.json()); //should be added BEFORE the endpoints, otherwise all the request bodies will be undefined!
 
+// Endpoints
 server.use("/authors", authorsRouter);
+server.use("/blogPosts", blogPostsRouter);
+
+// Error Handlers
+server.use(badRequestHandler); // 400
+server.use(unauthorizedHandler); // 401
+server.use(notFoundHandler); // 404
+server.use(genericErrorHandler); // it must be at the end of error handlers
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
