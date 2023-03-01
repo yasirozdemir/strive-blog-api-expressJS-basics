0165b97310d79fa7508368a85f03c9bb580a98cb
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-01 16:14:20 +0300

    error handlers added

---
+++ errorHandlers.js
@@ -0,0 +1,34 @@
+export const badRequestHandler = (err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({
+      success: false,
+      message: err.message,
+    });
+  } else next(err);
+};
+
+export const unauthorizedHandler = (err, req, res, next) => {
+  if (err.status === 401) {
+    res.status(401).send({
+      success: false,
+      message: err.message,
+    });
+  } else next(err);
+};
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({
+      success: false,
+      message: err.message,
+    });
+  } else next(err);
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log("ERROR: ", err.message);
+  res.status(500).send({
+    success: false,
+    message: "A problem occured caused by the server!",
+  });
+};
